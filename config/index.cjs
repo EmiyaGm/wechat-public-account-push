cb2c2cde7c9c36735ce1cf082f489167b7f49e81
EmiyaGm <464723943@qq.com>
2022-10-14 09:39:09 +0800

    fix(wechat-public-account-push): config

--- config/index.cjs
+++ config/index.cjs
@@ -2,66 +2,53 @@
 
 /**
  * 此项目配置为方便新人使用，已缩减至最简配置。
- * 如若想使用更多功能，请查考文档中的 【3. config参数说明】 
+ * 如若想使用更多功能，请查考文档中的 【3. config参数说明】
  * 自行添加属性，以支持更多个性化功能
  */
 const USER_CONFIG = {
-
-  // 使用微信测试号：公众号APP_ID
   APP_ID: '',
-
-  // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '',
-
-  PROVINCE: '广东',
-  CITY: '惠州',
-
+  IS_SHOW_COLOR: true,
+  CALLBACK_TEMPLATE_ID: '0-HIOIipj9tmjLqoLsMlvKx_cbYnTksnSXCcr3Cp0KE',
+  CALLBACK_USERS: [{ name: '自己', id: 'ok4Ta5_3jaGzlTETHX3v6NtM5FQc' }],
   USERS: [
     {
-      // 想要发送的人的名字
-      name: '宝贝',
-      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
-      // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
-      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
-      festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
-        },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
-        },
-      ],
-      // 我们在一起已经有xxxx天了的配置
-      customizedDateList: [
-        // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
-      ],
+      name: 'EmiyaGm',
+      id: 'ok4Ta5_3jaGzlTETHX3v6NtM5FQc',
+      useTemplateId: 'b_Q9JBza_SseajvfiCbdNiGglQDsiUmouzRGbbKkrPE',
+      province: '江苏',
+      city: '南京',
+      horoscopeDate: '12-14',
+      horoscopeDateType: '今日',
+      openUrl: 'https://github.com/EmiyaGm',
+      festivals: [{ type: '生日', name: '生日', date: '12-14', year: '1994' }],
+      customizedDateList: [{ keyword: '在一起的日子', date: '2022-07-30' }],
+    },
+    {
+      name: '66',
+      id: 'ok4Ta50JKShWbJwIe9pQ-nE4xRas',
+      useTemplateId: 'fNSsFKl01Ymg7NzYWN3rCqECLVDHTIBuO_-5f-Sb-pI',
+      province: '江苏',
+      city: '南京',
+      horoscopeDate: '08-16',
+      horoscopeDateType: '今日',
+      openUrl: 'https://github.com/EmiyaGm',
+      festivals: [{ type: '生日', name: '生日', date: '08-16', year: '1994' }],
+      customizedDateList: [{ keyword: '在一起的日子', date: '2022-07-30' }],
     },
   ],
-
-
-  // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
-
-  CALLBACK_USERS: [
-    {
-      name: '自己',
-      // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
-    }
-  ],
-
+  SWITCH: {
+    weather: true,
+    holidaytts: true,
+    CIBA: false,
+    oneTalk: false,
+    earthyLoveWords: false,
+    momentCopyrighting: false,
+    poisonChickenSoup: false,
+    poetry: false,
+    horoscope: false,
+    birthdayMessage: true,
+  },
 }
 
 module.exports = USER_CONFIG
-
